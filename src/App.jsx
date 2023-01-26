ea47e3cb518d7187c3631d5f797ff12e8be1dcca
Wiolla55525 <violeta.vinogrodska@gmail.com>
2023-01-26 23:17:06 +0200

    feat: :construction: Beginning of the project

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Header } from './Components/Header/Header.jsx';
+import { Nav } from "./Components/Nav/Nav";
+import { About } from "./Components/About/About";
+import { Experience } from "./Components/Experience/Experience";
+import { Services } from "./Components/Services/Services";
+import { Portfolio } from "./Components/Portfolio/Portfolio";
+import { Contact } from "./Components/Contact/Contact";
+import { Footer } from "./Components/Footer/Footer";
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Nav/>
+      <About/>
+      <Experience/>
+      <Services/>
+      <Portfolio/>
+      <Contact/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
