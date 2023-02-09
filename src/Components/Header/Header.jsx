62a0d26ee6cc770a399bd0a99e6ddb0ff082b301
Wiolla55525 <violeta.vinogrodska@gmail.com>
2023-02-09 14:58:13 +0200

    chore: :construction: header update

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 export const Header = () => {
   return (
     <div>
-      Header
+       <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Violeta</h1>
+        <div className="text-light">Front-end developer</div>
+       </div>
     </div>
   )
 }
