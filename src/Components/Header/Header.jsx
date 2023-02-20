039036856a1ac0f6d1c548040fff80d0f225650f
Wiolla55525 <violeta.vinogrodska@gmail.com>
2023-02-20 19:21:25 +0200

    feat: :sparkles: building header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import {Component} from './Component';
-import photo from '../../Assets/photo.jpg'
+import photo from '../../Assets/photo.jpg';
+import Socials from './Socials';
+import './header.css';
 
 export const Header = () => {
   return (
-    <div>
+    <div className='header'>
        <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Violeta</h1>
         <div className="text-light">Front-end developer</div>
         <Component/>
+        <Socials/>
 
         <div className="me">
-          <img src={photo} alt=''/>
+          {/* <img src={photo} alt=''/> */}
         </div>
 
         <a href='#contact' className='scroll_down'>Scroll Down</a>
--- src/Components/Header/Socials.jsx
+++ src/Components/Header/Socials.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {BsLinkedin} from 'react-icons/bs';
+import {BsLinkedin, BsGithub} from 'react-icons/bs';
 
 const Socials = () => {
   return (
     <div className='header_socials'>
-      <a href='https://www.linkedin.com/in/vvinogrodska 321' target='_blank'></a>
-      <a href='https://www.github.com/Wiolla55525' target='_blank'></a>
+      <a href='https://www.linkedin.com/in/vvinogrodska321' target='_blank'><BsLinkedin/></a>
+      <a href='https://www.github.com/Wiolla55525' target='_blank'><BsGithub/></a>
     </div>
   )
 }
