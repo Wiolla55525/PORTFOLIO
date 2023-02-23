8e67489cc738e73e53cb3d4e903ed5ca6168920a
Wiolla55525 <violeta.vinogrodska@gmail.com>
2023-02-23 11:51:14 +0200

    feat: :sparkles: building and styling nav

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -3,13 +3,15 @@ import './nav.css';
 import {AiOutlineHome, AiOutlineUser} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
 import {MdOutlineMessage} from 'react-icons/md'
-import {RiServiceLine} from 'react-icons/ri'
+import {RiServiceLine} from 'react-icons/ri';
+import { useState } from 'react';
 
 export const Nav = () => {
+  const [ activeNav, setActiveNav ] = useState('#')
   return (
     <nav>
-      <a href='#'> <AiOutlineHome/></a>
-      <a href='#about'> <AiOutlineUser/></a>
+      <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : '' }> <AiOutlineHome/></a>
+      <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : '' }> <AiOutlineUser/></a>
       <a href='#experience'> <BiBook/></a>
       <a href='#services'> <RiServiceLine/></a>
       <a href='#contact'> <MdOutlineMessage/></a>
