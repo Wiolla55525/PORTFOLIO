9febf90317253006c5b1a97fd44e2203643809aa
Wiolla55525 <violeta.vinogrodska@gmail.com>
2023-10-12 21:26:20 +0300

    contact construction

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
-import './contact.css'
+import './contact.css';
+import {MdOutlineEmail} from 'react-icons/md'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsWhatsapp} from 'react-icons/bs'
+
 
 export const Contact = () => {
   return (
     <section id='contact'>
-      Contact
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail/>
+            <h4>Email</h4>
+            <h5>violeta.vinogrodska@gmail.com</h5>
+            <a href="mailto:violeta.vinogrodska@gmail.com">Send a message</a>
+
+          </article>
+          <article className="contact__option">
+            <BsLinkedin/>
+            <h4>LinkedIn</h4>
+            <a href="https://m.me/vvinogrodska321">Send a message</a>
+
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp/>
+            <h4>Email</h4>
+            <h5>violeta.vinogrodska@gmail.com</h5>
+            <a href="mailto:violeta.vinogrodska@gmail.com">Send a message</a>
+
+          </article>
+        </div>
+
+        <form action=''></form>
+      </div>
+
     </section>
   )
 }
--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -4,8 +4,40 @@ import IMG1 from '../../Assets/Portfolio1.jpg'
 import IMG2 from '../../Assets/Portfolio2.jpg'
 import IMG3 from '../../Assets/Portfolio3.jpg'
 import IMG4 from '../../Assets/Portfolio4.jpg'
-import IMG5 from '../../Assets/Portfolio5.jpg'
-import IMG6 from '../../Assets/Portfolio6.jpg'
+
+
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Portfolio 1',
+    github: 'https://github.com/wiolla55525',
+    demo: 'https://github.com'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Portfolio 2',
+    github: 'https://github.com/wiolla55525',
+    demo: 'https://github.com'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Portfolio 3',
+    github: 'https://github.com/wiolla55525',
+    demo: 'https://github.com'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Portfolio 4',
+    github: 'https://github.com/wiolla55525',
+    demo: 'https://github.com'
+  },
+]
+
 
 export const Portfolio = () => {
   return (
@@ -13,46 +45,22 @@ export const Portfolio = () => {
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
       <div className="container portfolio__container">
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt=''></img>
-          </div>
-          <h3>This is a potfolio item title</h3>
-          <div className="portfolio__item-cta">
-              <a href='https://github.com/' className='btn' target='_blank'>Github</a>
-              <a href='https://dribbble.com/Alien_pixels/' className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt=''></img>
-          </div>
-          <h3>This is a potfolio item title</h3>
-          <div className="portfolio__item-cta">
-              <a href='https://github.com/' className='btn' target='_blank'>Github</a>
-              <a href='https://dribbble.com/Alien_pixels/' className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt=''></img>
-          </div>
-          <h3>This is a potfolio item title</h3>
-          <div className="portfolio__item-cta">
-              <a href='https://github.com/' className='btn' target='_blank'>Github</a>
-              <a href='https://dribbble.com/Alien_pixels/' className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt=''></img>
-          </div>
-          <h3>This is a potfolio item title</h3>
-          <div className="portfolio__item-cta">
-              <a href='https://github.com/' className='btn' target='_blank'>Github</a>
-              <a href='https://dribbble.com/Alien_pixels/' className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className='portfolio__item'>
+                  <div className="portfolio__item-image">
+                    <img src={image} alt={title}></img>
+                  </div>
+                  <h3>{title}</h3>
+                  <div className="portfolio__item-cta">
+                      <a href={github} className='btn' target='_blank' rel='noreferrer'>Github</a>
+                      <a href={demo} className='btn btn-primary' target='_blank' rel='noreferrer'>Live Demo</a>
+                  </div>
+              </article>
+            )
+          })
+        }
       </div>
     </section>
   )
